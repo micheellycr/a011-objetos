baf8767f1c7db14f266f92c589ce5831871d74c2
unknown <micheellycr@gmail.com>
2022-08-01 23:20:43 -0300

    resolução exercicio a011

---
+++ exercicio1/script.js
@@ -0,0 +1,25 @@
+const sacolao = []
+
+const fruta1 = { 
+    nome: "banana",
+preco: 3,
+disponibilidade: true,
+}
+
+const fruta2 = {
+    nome: "maça",
+preco: 6,
+disponibilidade: false,
+}
+
+const fruta3 = {
+    nome: "uva",
+preco: 9,
+disponibilidade: true,
+}
+
+sacolao.push(fruta1)
+sacolao.push(fruta2)
+sacolao.push(fruta3)
+
+console.log(sacolao)
